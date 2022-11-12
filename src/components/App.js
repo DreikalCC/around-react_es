a16e4d0944c26ad9a1bdb6104fe1962533d36190
Aldo Villegas <aldoaccs@yahoo.com.mx>
2022-11-12 12:34:17 -0800

    corrected as per comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import { Main } from './Main';
 import { ImagePopup } from './ImagePopup';
+import { PopupWithForm } from './PopupWithForm';
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
@@ -48,13 +49,6 @@ function App() {
       <Header />
 
       <Main
-        isEditAvatarOpen={isEditAvatarPopupOpen}
-        isEditProfileOpen={isEditProfilePopupOpen}
-        isAddPlaceOpen={isAddPlacePopupOpen}
-        isEraseCardOpen={isEraseCardPopupOpen}
-        isImageOpen={isImagePopupOpen}
-        selectedCard={selectedCard}
-        handleClose={closeAllPopups}
         handleCardClick={handleCardClick}
         handleEditAvatarClick={handleEditAvatarClick}
         handleEditProfileClick={handleEditProfileClick}
@@ -66,6 +60,102 @@ function App() {
         isPopupOpen={isImagePopupOpen}
         onClose={closeAllPopups}
       />
+
+      <PopupWithForm
+        name="profile"
+        title="Editar perfil"
+        buttonText="Guardar"
+        isPopupOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="input__field">
+          <input
+            id="profile-name-input"
+            name="name"
+            required
+            type="text"
+            placeholder="Jaques Cousteau"
+            className="input__form input__name"
+            minLength="2"
+            maxLength="40"
+          />
+          <span className="input__form-error profile-name-input-error"></span>
+        </label>
+        <label className="input__field">
+          <input
+            id="profile-desc-input"
+            name="about"
+            required
+            type="text"
+            placeholder="Explorador"
+            className="input__form input__description"
+            minLength="2"
+            maxLength="200"
+          />
+          <span className="input__form-error profile-desc-input-error"></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="gallery"
+        title="Nuevo lugar"
+        buttonText="Crear"
+        isPopupOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="input__field">
+          <input
+            required
+            id="card-name-input"
+            name="name"
+            type="text"
+            placeholder="Título"
+            className="input__form input__name input__name_gallery"
+            minLength="2"
+            maxLength="30"
+          />
+          <span className="input__form-error card-name-input-error"></span>
+        </label>
+        <label className="input__field">
+          <input
+            required
+            id="card-url-input"
+            name="link"
+            type="url"
+            placeholder="URL de la imagen"
+            className="input__form input__description"
+          />
+          <span className="input__form-error card-url-input-error"></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="avatar"
+        title="Cambiar foto de perfil"
+        buttonText="Guardar"
+        isPopupOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <label className="input__field">
+          <input
+            required
+            id="avatar-url-input"
+            name="link"
+            type="url"
+            placeholder="URL de la imagen"
+            className="input__form input__description input__description_gallery"
+          />
+          <span className="input__form-error avatar-url-input-error"></span>
+        </label>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="eraser"
+        title="¿Estás seguro?"
+        buttonText="Si"
+        isPopupOpen={isEraseCardPopupOpen}
+        onClose={closeAllPopups}
+      ></PopupWithForm>
 
       <Footer />
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { PopupWithForm } from './PopupWithForm';
-import { ImagePopup } from './ImagePopup';
 import { api } from '../utils/Api';
 import { Card } from './Card';
 
@@ -62,102 +60,6 @@ export function Main(props) {
           );
         })}
       </section>
-
-      <PopupWithForm
-        name="profile"
-        title="Editar perfil"
-        buttonText="Guardar"
-        isPopupOpen={props.isEditProfileOpen}
-        onClose={props.handleClose}
-      >
-        <label className="input__field">
-          <input
-            id="profile-name-input"
-            name="name"
-            required
-            type="text"
-            placeholder="Jaques Cousteau"
-            className="input__form input__name"
-            minLength="2"
-            maxLength="40"
-          />
-          <span className="input__form-error profile-name-input-error"></span>
-        </label>
-        <label className="input__field">
-          <input
-            id="profile-desc-input"
-            name="about"
-            required
-            type="text"
-            placeholder="Explorador"
-            className="input__form input__description"
-            minLength="2"
-            maxLength="200"
-          />
-          <span className="input__form-error profile-desc-input-error"></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        name="gallery"
-        title="Nuevo lugar"
-        buttonText="Crear"
-        isPopupOpen={props.isAddPlaceOpen}
-        onClose={props.handleClose}
-      >
-        <label className="input__field">
-          <input
-            required
-            id="card-name-input"
-            name="name"
-            type="text"
-            placeholder="Título"
-            className="input__form input__name input__name_gallery"
-            minLength="2"
-            maxLength="30"
-          />
-          <span className="input__form-error card-name-input-error"></span>
-        </label>
-        <label className="input__field">
-          <input
-            required
-            id="card-url-input"
-            name="link"
-            type="url"
-            placeholder="URL de la imagen"
-            className="input__form input__description"
-          />
-          <span className="input__form-error card-url-input-error"></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        name="avatar"
-        title="Cambiar foto de perfil"
-        buttonText="Guardar"
-        isPopupOpen={props.isEditAvatarOpen}
-        onClose={props.handleClose}
-      >
-        <label className="input__field">
-          <input
-            required
-            id="avatar-url-input"
-            name="link"
-            type="url"
-            placeholder="URL de la imagen"
-            className="input__form input__description input__description_gallery"
-          />
-          <span className="input__form-error avatar-url-input-error"></span>
-        </label>
-      </PopupWithForm>
-
-      <PopupWithForm
-        name="eraser"
-        title="¿Estás seguro?"
-        buttonText="Si"
-        isPopupOpen={props.isEraseCardOpen}
-        onClose={props.handleClose}
-      ></PopupWithForm>
     </>
   );
 }
