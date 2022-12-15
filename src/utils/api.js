bfefe0017b04f36b7bace5f9985687deefc4688d
Aldo Villegas <avgaccs@gmail.com>
2022-12-15 12:51:01 -0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -88,7 +88,7 @@ class Api {
   }
 }
 
-export const api = new Api({
+const api = new Api({
   address: 'https://around.nomoreparties.co/v1/',
   groupId: 'web_es_cohort_02',
   token: '04346056-dea4-4d40-8541-43203e80bf1a',
@@ -97,3 +97,5 @@ export const api = new Api({
     'Content-Type': 'application/json',
   },
 });
+
+export default api;
